7e3be1c7f2fae9a7f28fb42429b589d7a4368211
sobird <i@sobird.me>
2023-11-29 12:14:46 +0800

    update

--- models/index.ts
+++ models/index.ts
@@ -1,21 +1,23 @@
 /**
  * models
  *
+ * import { models } from '@auth/sequelize-adapter';
+ *
  * @see https://github.com/sequelize/sequelize/blob/main/types/test/typescriptDocs/ModelInit.ts
  *
  * sobird<i@sobird.me> at 2021/11/16 20:33:20 created.
  */
 
-import { models } from '@auth/sequelize-adapter';
 import sequelize from '@/lib/sequelize';
 import User from './user';
 import Account from './account';
+import Session from './session';
+import VerificationToken from './verificationToken';
 
-// export const UserModel = User;
-const defaultModelOptions = { underscored: false, timestamps: false };
-
-const Session = sequelize.define('Session', {}, defaultModelOptions);
-const VerificationToken = sequelize.define('VerificationToken', models.VerificationToken, defaultModelOptions);
+export const UserModel = User;
+export const AccountModel = Account;
+export const SessionModel = Session;
+export const VerificationTokenModel = VerificationToken;
 
 Account.belongsTo(User, { onDelete: 'cascade' });
 Session.belongsTo(User, { onDelete: 'cascade' });
