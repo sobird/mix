fc9ee98abd50491de8f4f6d2612b51e6232fa4b7
sobird <i@sobird.me>
2024-11-21 00:50:07 +0800

    chore: use umzug

--- test.ts
+++ test.ts
@@ -1,10 +1,17 @@
-interface Person {
-  name: string;
-  age: number;
-  email?: string;
-}
+// index.js
+import { Sequelize } from 'sequelize';
+import { Umzug, SequelizeStorage } from 'umzug';
 
-type PersonKeys = keyof Person; // "name" | "age" | "email"
+const sequelize = new Sequelize({ dialect: 'sqlite', storage: './db.sqlite' });
 
-const P: PersonKeys = 'age';
-console.log('P', P);
+const umzug = new Umzug({
+  migrations: { glob: 'migrations/*.js' },
+  context: sequelize.getQueryInterface(),
+  storage: new SequelizeStorage({ sequelize }),
+  logger: console,
+});
+
+// Checks migrations and run them if they are not already applied. To keep
+// track of the executed migrations, a table (and sequelize model) called SequelizeMeta
+// will be automatically created (if it doesn't exist already) and parsed.
+await umzug.up();
