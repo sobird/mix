95f41c3f6f60d7b0010621be155a36a33e7f4955
sobird <i@sobird.me>
2024-11-20 22:44:17 +0800

    chore: deploy workflow manually

--- pm2.config.js
+++ pm2.config.js
@@ -10,21 +10,21 @@ module.exports = {
     /** application name (default to script filename without extension) */
     name: 'mix',
     /** script path relative to pm2 start */
-    script: 'standalone/server.js',
+    script: 'server.js',
     // args: '-p 3000',
     /** the directory from which your app will be launched */
     // cwd: "",
     /** true by default. if false, PM2 will not restart your app if it crashes or ends peacefully */
     autorestart: true,
     /** enable watch & restart feature, if a file change in the folder or subfolder, your app will get reloaded */
-    watch: false,
+    // watch: false,
     /** Delay between restart */
-    watch_delay: 1000,
+    // watch_delay: 1000,
     /** list of regex to ignore some file or folder names by the watch feature */
-    ignore_watch: [
-      'public',
-      'node_modules',
-    ],
+    // ignore_watch: [
+    //   'public',
+    //   'node_modules',
+    // ],
     /**
      * PM2 is giving chokidar these Default options:
      * watch_options: {
@@ -33,34 +33,34 @@ module.exports = {
      * }
      * @see https://github.com/paulmillr/chokidar#api
      */
-    watch_options: {
-      followSymlinks: false,
-    },
+    // watch_options: {
+    //   followSymlinks: false,
+    // },
     /** The interpreter used. */
-    exec_interpreter: 'bash',
+    // exec_interpreter: 'bash',
     /** alias to interpreter_args */
     // node_args: "--harmony",
     /**
      * 启用多少个实例，可用于负载均衡
      * 如果设为 0或者 max，则根据当前机器核数确定实例数目。
      */
-    instances: 0,
+    // instances: 0,
     /** mode to start your app, can be “cluster” or “fork”, default fork */
-    exec_mode: 'cluster_mode',
+    // exec_mode: 'cluster_mode',
 
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    // log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
   }],
 
-  deploy: {
-    production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
-      ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-    },
-  },
+  // deploy: {
+  //   production: {
+  //     user: 'SSH_USERNAME',
+  //     host: 'SSH_HOSTMACHINE',
+  //     ref: 'origin/master',
+  //     repo: 'GIT_REPOSITORY',
+  //     path: 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': '',
+  //   },
+  // },
 };
